9613ec73559c01329b92b75ce90a10ba6e484277
Adaspeaks <adaspeaks@gmail.com>
2021-07-05 16:09:48 -0700

    added basic abilty to read tables

--- index.js
+++ index.js
@@ -1,51 +1,80 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const cTable = require ("console.table");
+const cTable = require("console.table");
 const connection = require("./db/connection");
 
-function init(){
-    inquirer.prompt({
-        type: "list",
-        name: "mainMenu",
-        message: "What action would you like to preform?",
-        choices: [
-            "View Departments",
-            "View Roles",
-            "View Employees",
-            "View Employees by Manager",
-            "Add Department",
-            "Add Role",
-            "Add Employee",
-            "Update Employee's Roles",
-            "END TASK" 
-        ],
-      }).then(function(data) {
-        if(data.option === "View Departments") {
-           readDepartment();
-        } else if (data.option ===  "View Roles") {
-           readRoles();
-        } else if (data.option === "View Employees") {
-           readEmployees();
-        } else if (data.option === "View Employees by Manager") {
-           readEmployeesMng();
-        } else if (data.option === "Add Department") {
-           writeDepartment();
-        } else if (data.option === "Add Roles") {
-           writeRole();
-        } else if (data.option === "Add Employee") {
-           writeEmployee();
-        } else if (data.option === "Update Employee's Roles") {
-           updateRole();
-        } else if (data.option === "Remove Employee") {
-           deleteEmployee();
-        } else {
-           console.log("OK,Good bye!");
-           process.exit(0);
-        };
+function init() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "mainMenu",
+      message: "What action would you like to preform?",
+      choices: [
+        "View Departments",
+        "View Roles",
+        "View Employees",
+        "View Employees by Manager",
+        "Add Department",
+        "Add Role",
+        "Add Employee",
+        "Update Employee's Roles",
+        "END TASK",
+      ],
+    })
+    .then(function (data) {
+      if (data.mainMenu === "View Departments") {
+        readDepartment();
+      } else if (data.mainMenu === "View Roles") {
+        readRoles();
+      } else if (data.mainMenu === "View Employees") {
+        readEmployees();
+      } else if (data.mainMenu === "View Employees by Manager") {
+        readEmployeesMng();
+      } else if (data.mainMenu === "Add Department") {
+        writeDepartment();
+      } else if (data.mainMenu === "Add Roles") {
+        writeRole();
+      } else if (data.mainMenu === "Add Employee") {
+        writeEmployee();
+      } else if (data.mainMenu === "Update Employee's Roles") {
+        updateRole();
+      } else if (data.mainMenu === "Remove Employee") {
+        deleteEmployee();
+      } else {
+        console.log("OK,Good bye!");
+        process.exit(0);
+      }
     });
- };
+}
 
-connection.connect((err) =>{
+function readDepartment() {
+  connection.query("SELECT * FROM department", (err, data) => {
     if (err) throw err;
+    console.log("=== Departments ===");
+    console.table(data);
     init();
+  });
+}
+
+function readRoles() {
+  connection.query("SELECT * FROM role", (err, data) => {
+    if (err) throw err;
+    console.log("===================== Roles =====================");
+    console.table(data);
+    init();
+  });
+}
+
+function readEmployees() {
+  connection.query("SELECT * FROM employee", (err, data) => {
+    if (err) throw err;
+    console.log("================== Employees =================");
+    console.table(data);
+    init();
+  });
+}
+
+connection.connect((err) => {
+  if (err) throw err;
+  init();
 });
