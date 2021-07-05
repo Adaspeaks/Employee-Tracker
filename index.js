5f2f06ccc06ede488e7c1f73dc7052780e7cb195
Adaspeaks <adaspeaks@gmail.com>
2021-07-05 15:28:34 -0700

    set up base prompt

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const connection = require("./connection");
+const cTable = require ("console.table");
+const connection = require("./db/connection");
 
 function init(){
-
-}
+    inquirer.prompt({
+        type: "list",
+        name: "mainMenu",
+        message: "What action would you like to preform?",
+        choices: [
+            "View Departments",
+            "View Roles",
+            "View Employees",
+            "View Employees by Manager",
+            "Add Department",
+            "Add Role",
+            "Add Employee",
+            "Update Employee's Roles",
+            "END TASK" 
+        ],
+      }).then(function(data) {
+        if(data.option === "View Departments") {
+           readDepartment();
+        } else if (data.option ===  "View Roles") {
+           readRoles();
+        } else if (data.option === "View Employees") {
+           readEmployees();
+        } else if (data.option === "View Employees by Manager") {
+           readEmployeesMng();
+        } else if (data.option === "Add Department") {
+           writeDepartment();
+        } else if (data.option === "Add Roles") {
+           writeRole();
+        } else if (data.option === "Add Employee") {
+           writeEmployee();
+        } else if (data.option === "Update Employee's Roles") {
+           updateRole();
+        } else if (data.option === "Remove Employee") {
+           deleteEmployee();
+        } else {
+           console.log("OK,Good bye!");
+           process.exit(0);
+        };
+    });
+ };
 
 connection.connect((err) =>{
     if (err) throw err;
